9f19b61fc0d6d913be6de78e8f9d37588d3892fb
hhorsekim <hhorsekim@gmail.com>
2022-04-26 21:50:07 +0900

    [ADD] Client Footer, Nav, Signup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import Signup from './Pages/Signup';
+import Main from './Pages/Main';
 
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Nav />
+
+      <div>==여기 바디==</div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Pages/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPage() {
+  return <div>여긴 메인</div>;
+}
+
+export default MainPage;
