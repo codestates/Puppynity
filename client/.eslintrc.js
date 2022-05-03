b104d19fdeb7e094be09c994370f0c2f9c513c8d
bellamy828 <72433598+bellamy828@users.noreply.github.com>
2022-05-03 13:12:20 +0900

    Revert "[ADD] siginup"

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,31 +3,16 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react-hooks'],
   extends: [
     'airbnb', // or airbnb-base
-    'prettier',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended', // 설치 한경우
     'plugin:import/errors', // 설치한 경우
     'plugin:import/warnings', // 설치한 경우
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'plugin:import/typescript',
   ],
   rules: {
     'prettier/prettier': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'no-use-before-define': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        mjs: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-        svg: 'never',
-      },
-    ],
   },
   settings: {
     'import/resolver': {
--- client/src/Redux/contentSlice.ts
+++
@@ -1,2 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-import configureStore from './configureStore';
