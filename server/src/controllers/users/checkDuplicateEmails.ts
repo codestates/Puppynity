ecf3b3b4eb9e365444ee4689f3afde3f6e21951f
JeongTaeYeong <bellamy828@gmail.com>
2022-05-04 19:35:34 +0900

    [ADD]Server duplicate email check

---
+++ server/src/controllers/users/checkDuplicateEmails.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { User } from '../../entity/User';
+
+export const checkDuplicateEmails = async (req: Request, res: Response) => {
+  console.log('이메일 중복 체크 🕹');
+  const { email } = req.body;
+
+  try {
+    const userInfo = await User.findOne({ email });
+    console.log('이메일중복체크유저정보', userInfo);
+    if (!userInfo) {
+      return res.status(200).json({ message: '사용 가능한 이메일입니다.' });
+    }
+    res.status(200).json({ message: '이미 회원가입한 이메일입니다.' });
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
